0b29478c1558dac1074d773cebc3f0a6bfa45665
Jimmy Nguyen <jinguen@ucsd.edu>
2016-04-15 20:07:30 -0700

    catch up

--- main.cpp
+++ main.cpp
@@ -58,6 +58,66 @@ makeSpiralScene()
     g_scene->preCalc();
 }
 
+void
+makeSphereScene()
+{
+	g_camera = new Camera;
+	g_scene = new Scene;
+	g_image = new Image;
+
+	g_image->resize(512, 512);
+
+	//Set up camera
+	g_camera->setBGColor(Vector3(0.0f, 0.0f, 0.2f));
+	g_camera->setEye(Vector3(-2, 3, 5));
+	g_camera->setLookAt(Vector3(-.5, 1, 0));
+	g_camera->setUp(Vector3(0, 1, 0));
+	g_camera->setFOV(45);
+
+	// create and place a point light source
+	PointLight * light = new PointLight;
+	light->setPosition(Vector3(-3, 15, 3));
+	light->setColor(Vector3(1, 1, 1));
+	light->setWattage(500);
+	g_scene->addLight(light);
+
+	Material* mat = new Lambert(Vector3(1.0f));
+
+	/*
+	TriangleMesh * sphere = new TriangleMesh;
+	sphere->load("models/sphere.obj");
+
+	// create all the triangles in the sphere mesh and add to the scene
+	for (int i = 0; i < sphere->numTris(); ++i)
+	{
+		Triangle* t = new Triangle;
+		t->setIndex(i);
+		t->setMesh(sphere);
+		t->setMaterial(mat);
+		g_scene->addObject(t);
+	}
+	*/
+
+	// create the floor triangle
+	TriangleMesh * floor = new TriangleMesh;
+	floor->createSingleTriangle();
+	floor->setV1(Vector3(0, 0, 10));
+	floor->setV2(Vector3(10, 0, -10));
+	floor->setV3(Vector3(-10, 0, -10));
+	floor->setN1(Vector3(0, 1, 0));
+	floor->setN2(Vector3(0, 1, 0));
+	floor->setN3(Vector3(0, 1, 0));
+
+	Triangle* t = new Triangle;
+	t->setIndex(0);
+	t->setMesh(floor);
+	t->setMaterial(mat);
+	g_scene->addObject(t);
+
+	// let objects do pre-calculations if needed
+	g_scene->preCalc();
+}
+
 
 void
 makeBunnyScene()
@@ -85,7 +145,7 @@ makeBunnyScene()
     Material* mat = new Lambert(Vector3(1.0f));
 
     TriangleMesh * bunny = new TriangleMesh;
-    bunny->load("bunny.obj");
+    bunny->load("models/bunny.obj");
     
     // create all the triangles in the bunny mesh and add to the scene
     for (int i = 0; i < bunny->numTris(); ++i)
@@ -123,6 +183,8 @@ main(int argc, char*argv[])
 {
     // create a scene
     makeSpiralScene();
+	//makeBunnyScene();
+	//makeSphereScene();
 
     MiroWindow miro(&argc, argv);
     miro.mainLoop();
